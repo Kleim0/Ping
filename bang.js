7e477bceddb8c0fb0ea0f8d6b87e870789a1e5e6
Kleim0 <100975793+Kleim0@users.noreply.github.com>
2023-05-28 04:54:41 +0000

    fixed the shit physics

--- bang.js
+++ bang.js
@@ -1,27 +1,32 @@
-var penis = 0;
-
 const canvas = document.getElementById("canvas"); // gay shit
 const c = canvas.getContext("2d");
 const popup = document.getElementById("start");
+const leftScore = document.getElementById("leftScore");
+const rightScore = document.getElementById("rightScore");
 const paddleH = 40;
-const paddleW = 4;
+const paddleW = 5;
 let paddleC = "#191a1a"
 let quarkC = "#f0f6f0"
 let quarkR = 5;
-let speed = 3;
+let speed = 2;
 
 canvas.width = window.innerWidth * 0.50; // canvas declarations
 canvas.height = window.innerHeight * 0.50;
 const cw = canvas.width;
 const ch = canvas.height;
 canvas.style.display = "none" // dissapere like ur dad
+rightScore.style.display = "none"
+leftScore.style.display = "none"
 
 let paused = true;
 
 function getMilk() { // basically just starting ong fam just like minecraft new world
     popup.style.display = "none";
     canvas.style.display = "block"
+		rightScore.style.display = "block"
+		leftScore.style.display = "block"
     paused = false;
+		quark.reset()
     requestAnimationFrame(doShit);
 }
 
@@ -33,55 +38,75 @@ function middleGuy() { // line in da middle
     c.moveTo(cw/2, 0);
     c.lineTo(cw/2, ch);
     c.stroke();
-}
+} // dashed line in da middle
 
 let leftPaddle = {
-    x: paddleH/2,
-    y: ch/2 - paddleH/2,
-    speed: 0,
-    width: paddleW,
-    height: paddleH,
+	x: paddleH/2,
+	y: ch/2 - paddleH/2,
+	speed: 0,
+	width: paddleW,
+	height: paddleH,
+	up: false,
+	down: false,
+	score: 0,
 
-    draw: function(){
-        c.fillStyle = paddleC;
-        c.beginPath();
-        c.rect(this.x, this.y, this.width, this.height);
-        c.fill();
-        c.closePath();
-    },
+	draw: function(){
+			c.fillStyle = paddleC;
+			c.beginPath();
+			c.rect(this.x, this.y, this.width, this.height);
+			c.fill();
+			c.closePath();
+	},
 
-    update: function(){
-        this.y += this.speed;
-        this.draw()
-    },
+	update: function(){
+		if (this.up && this.y > 1) {
+			this.speed = -speed;
+		} else if (this.down && this.y + this.height < ch -1) {
+			this.speed = speed;
+		} else {
+			this.speed = 0;
+		}
+			this.y += this.speed;
+			this.draw()
+	},
 };
 
 let rightPaddle = {
-    x: cw - paddleH/2 - paddleW,
-    y: ch/2 - paddleH/2,
-    speed: 0,
-    width: paddleW,
-    height: paddleH,
+	x: cw - paddleH/2 - paddleW,
+	y: ch/2 - paddleH/2,
+	speed: 0,
+	width: paddleW,
+	height: paddleH,
+	up: false,
+	down: false,
+	score: 0,
 
-    draw: function(){
-        c.fillStyle = paddleC;
-        c.beginPath();
-        c.rect(this.x, this.y, this.width, this.height);
-        c.fill();
-        c.closePath();
-    },
+	draw: function(){
+			c.fillStyle = paddleC;
+			c.beginPath();
+			c.rect(this.x, this.y, this.width, this.height);
+			c.fill();
+			c.closePath();
+	},
 
-    update: function(){
-        this.y += this.speed;
-        this.draw()
-    },
+	update: function(){
+		if (this.up && this.y > 1) {
+			this.speed = -speed;
+		} else if (this.down && this.y + this.height < ch -1) {
+			this.speed = speed;
+		} else {
+			this.speed = 0;
+		}
+			this.y += this.speed;
+			this.draw()
+	},
 };
 
 let quark = {
     x: cw/2,
     y: ch/2,
-    dx: 5,
-    dy: 2,
+    dx: 0,
+    dy: 0,
     radius: quarkR,
     color: 0,
 
@@ -90,15 +115,26 @@ let quark = {
             this.dy = -this.dy;
         }
 
-        if ((this.x + this.radius + this.dx >= rightPaddle.x && this.x - this.radius <= rightPaddle.x + paddleW && this.y + this.dy > rightPaddle.y && this.y + this.dy < rightPaddle.y + paddleH || this.x - this.radius + this.dx <= leftPaddle.x + paddleW && this.x + this.radius >= leftPaddle.x && this.y + this.dy > leftPaddle.y && this.y + this.dy < leftPaddle.y + paddleH) && penis < 0) {
-            penis = 60;
+        if (this.x + this.radius + this.dx >= rightPaddle.x && this.y + this.dy > rightPaddle.y && this.y + this.dy < rightPaddle.y + paddleH && this.dx > 0) {
             this.dx = -this.dx;
-            console.log("bounce")
-        } else if (this.x + this.radius + this.dx >= cw) {
+						this.dx -= Math.abs(rightPaddle.speed/8);
+						this.dy += rightPaddle.speed/4
+            console.log("right bounce")
+        } else if (this.x - this.radius + this.dx <= leftPaddle.x + paddleW && this.y + this.dy > leftPaddle.y && this.y + this.dy < leftPaddle.y + paddleH && this.dx < 0) {
+            this.dx = -this.dx;
+						this.dx += Math.abs(leftPaddle.speed/8);
+						this.dy += leftPaddle.speed/4
+						
+            console.log("left bounce")
+				} else if (this.x + this.radius + this.dx >= cw) {
             console.log("Left wins")
+						leftPaddle.score += 1;
+						leftScore.innerHTML = leftPaddle.score;
             this.reset()
         } else if (this.x - this.radius + this.dx <= 0) {
             console.log("right wins")
+						rightPaddle.score += 1;
+						rightScore.innerHTML = rightPaddle.score;
             this.reset()
         }
 
@@ -119,61 +155,56 @@ let quark = {
         let k = Math.random() < 0.5 ? -1 : 1;
         this.x = cw/2;
         this.y = ch/2;
-        this.dx = (Math.random() * 2 + 1) * k;
-        this.dy = (Math.random() * 2 + 1) * k;
+        this.dx = 1.5 * k;
+        this.dy = 0;
     },
-}
+} // ball
 
-window.addEventListener("keydown", function(event){
-    switch (event.key) {
-        case "w":
-            leftPaddle.speed = -speed;
-            break;
-
-        case "s":
-            leftPaddle.speed = speed;
-            break;
-
-        case "ArrowUp":
-            rightPaddle.speed = -speed;
-            break;
-
-        case "ArrowDown":
-            rightPaddle.speed = speed;
-            break;
-    }
-
-})
-window.addEventListener("keyup", function(event){
-    switch (event.key) {
-        case "w":
-            leftPaddle.speed = 0;
-            break;
-
-        case "s":
-            leftPaddle.speed = 0;
-            break;
-
-        case "ArrowUp":
-            rightPaddle.speed = 0;
-            break;
-
-        case "ArrowDown":
-            rightPaddle.speed = 0;
-            break;
-    }
-})
-
+window.addEventListener("keydown", function(event) {
+	switch (event.key) {
+		case "w":
+			leftPaddle.up = true;
+			break;
+		case "s":
+			leftPaddle.down = true;
+			break;
+		case "ArrowUp":
+			rightPaddle.up = true;
+			break;
+		case "ArrowDown":
+			rightPaddle.down = true;
+			break;
+		case " ":
+			paused = !paused;
+			console.log(paused)
+	}
+}) // Keydown handler
+window.addEventListener("keyup", function(event) {
+	console.log(event)
+		switch (event.key) {
+		case "w":
+			leftPaddle.up = false;
+			break;
+		case "s":
+			leftPaddle.down = false;
+			break;
+		case "ArrowUp":
+			rightPaddle.up = false;
+			break;
+		case "ArrowDown":
+			rightPaddle.down = false;
+			break;
+		}
+}) // Keyup handler
 
 function doShit() {
-    if (!paused) {
+  if (!paused) {
         c.clearRect(0, 0, cw, ch)
+	      quark.update()
         rightPaddle.update()
         leftPaddle.update()
-        quark.update()
         middleGuy()
-        requestAnimationFrame(doShit);
-        penis -= 1;
-    }
-}
+       }
+	requestAnimationFrame(doShit);
+} //render function
 requestAnimationFrame(doShit);
